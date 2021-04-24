3d4ad649e86dc78df8cd366dda576d616b302972
Roman Borisov <riborisov@sbercloud.ru>
2021-04-24 15:50:40 +0300

    added tests for jsonplaceholder service

--- l6_api_testing/tests/conftest.py
+++ l6_api_testing/tests/conftest.py
@@ -1,5 +1,8 @@
 import pytest
 import json
+import requests
+from faker import Faker
+import random
 
 
 @pytest.fixture()
@@ -8,3 +11,23 @@ def all_dog_breeds():
         j = file.read()
         dogs = json.loads(j)
         return dogs
+
+
+fake = Faker()
+
+
+@pytest.fixture()
+def data_generator():
+    data = {
+        'title': fake.name(),
+        'body': fake.text(),
+        'userid': fake.random_int()
+    }
+    return data
+
+
+# возвращает случайный userId из списка существующих
+@pytest.fixture()
+def random_exist_userid():
+    r = requests.get('https://jsonplaceholder.typicode.com/users')
+    return random.randrange(0, (len(r.json())), 1)
